83b471a2cbe1f3a9fa208b084b75922660d59a60
isaicalderon <isai.acalderon@gmail.com>
2019-05-05 23:19:01 -0700

    v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
+import {AccordionModule} from 'primeng/accordion'; 
+import {MenuItem} from 'primeng/api'; 
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,8 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    FormsModule,
+    //MenuItem,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
