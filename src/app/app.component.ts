dc95ae354dd192ff37b6b9cb6294e4c124bedecf
isai.acalderon <isai.acalderon@gmail.com>
2019-06-07 17:48:03 -0700

    v1.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ManualWebAngular';
+export class AppComponent implements OnInit{
+  
+  menuNav:MenuItem[]; // p-breadcrumb
+
+  menuBar:MenuItem[]; // p-menubar
+
+  cols:any[];
+
+  ngOnInit(){
+
+    this.cols = [
+      { field: 'matricula', header: 'Matricula' },
+      { field: 'nombres', header: 'Nombre' },
+      { field: 'apellidoPaterno', header: 'Apellido Paterno' },
+      { field: 'apellidoMaterno', header: 'Apellido Materno' }
+    ];
+
+    this.menuBar = [
+      {
+          label: 'Opciones',
+          items: [
+            {
+              label: 'Agregar', 
+              icon: 'pi pi-fw pi-plus'
+            },
+            {
+              label: 'Editar', 
+              icon: 'pi pi-fw pi-plus',
+              disabled: true
+            },
+            {
+              label: 'Eliminar', 
+              icon: 'pi pi-fw pi-plus',
+              disabled: true
+            }
+          ]
+      }
+    ];
+
+    this.menuNav = [
+      {icon:'pi pi-home'},
+      {label:'Catálogo'},
+      {label:'Alumnos'}
+    ];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 import {AccordionModule} from 'primeng/accordion'; 
 import {MenuItem} from 'primeng/api'; 
 import { FormsModule } from '@angular/forms';
-
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+
+// componentes
+import {BreadcrumbModule} from 'primeng/breadcrumb';
+import {MenubarModule} from 'primeng/menubar';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,10 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     AccordionModule,
     FormsModule,
-    //MenuItem,
+    BreadcrumbModule,
+    MenubarModule,
+    TableModule
+    //MenuItem
   ],
 
   providers: [],
